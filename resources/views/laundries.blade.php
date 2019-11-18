ad7b0c3910c5bb3acffa0bd7061f7515dbe53aa6
tonhal <57263132+tonhal@users.noreply.github.com>
2019-11-18 11:33:05 +0100

    Initializing Fullcalendar and Bulma
    The start of using fullcalendar and bulma with their js and css files

--- resources/views/laundries.blade.php
+++ resources/views/laundries.blade.php
@@ -5,24 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Mosások</title>
+    <script src='{{url('/add-on/jquery-3.4.1.min.js')}}'></script>
+    <script src='{{url('/add-on/moment.js')}}'></script>
+    <script src='{{url('/add-on/fullcalendar.js')}}'></script>
+    <link rel="stylesheet" type="text/css" href="{{url('/css/fullcalendar.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{url('/css/bulma.css')}}">
 </head>
 <body>
-    <p>mosások</p>
-    <table>
-        <tr>
-            <td>Username</td>
-            <td>Start</td>
-            <td>End</td>
-            <td>Comment</td>
-        </tr>
-        @foreach ($laundries as $laundry)
-            <tr>
-                <td>{{ $laundry->username }}</td>
-                <td>{{ $laundry->from }}</td>
-                <td>{{ $laundry->to }}</td>
-                <td>{{ $laundry->comment }}</td>
-            </tr>
-        @endforeach
-    </table>
+    <h2 class='title is-2'>Mosások</h2>
+    <div id='calendar'></div>
+    <script>
+        $(function() {
+            $('#calendar').fullCalendar({
+                defaultView: 'agendaWeek'
+            })
+        });
+    </script>
 </body>
 </html>
