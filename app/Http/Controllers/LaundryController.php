083b396f7848a7837136f48091a247cac06960a4
tonhal <57263132+tonhal@users.noreply.github.com>
2019-11-18 19:28:40 +0100

    Lot of changes
    Ajax, Server-side validation, My Laundries, etc.

--- app/Http/Controllers/LaundryController.php
+++ app/Http/Controllers/LaundryController.php
@@ -4,15 +4,49 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use DateTime;
 
 class LaundryController extends Controller
 {
     public function index()
     {
         $laundries = DB::table('laundries')
-                        ->select('id','username as title','start','end')
-                        ->get();
+            ->select('id','username as title','start','end')
+            ->get();
+
+        $user_laundries = DB::table('laundries')
+            ->select('id','username','start','end')
+            ->where('username','=','agofekete')
+            ->whereRaw('DATE(start) >= DATE(NOW())')
+            ->orderByRaw('3')
+            ->get();
                
-        return view('laundries', compact('laundries'));
+        return view('laundries', compact('laundries','user_laundries'));
+    }
+
+    public function insert()
+    {
+        $username = request('username');
+        $start_time = new DateTime(request('start_time'));
+        $end_time = new DateTime(request('end_time'));
+        $comment = request('comment');
+        
+        $overlap = DB::table('laundries')
+            ->select('id')
+            ->where('start', '<', $end_time)
+            ->where('end', '>', $start_time)
+            ->count();
+
+        if($overlap != 0) {
+            return response()->json(['error' => 'Ennek az idősávnak bizonyos részére már foglalt a mosógép.'], 404);
+        } else if(!($start_time < $end_time)) { 
+            return response()->json(['error' => 'Valami nem stimmel az időpontokkal!'], 404);
+        } else {
+            DB::table('laundries')->insert([
+            'username' => $username, 'start' => $start_time, 'end' => $end_time, 'comment' => $comment
+            ]);
+
+            return response()->json(['success' => 'true']);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@
 */
 
 Route::get('/', 'LaundryController@index');
+Route::post('/newlaundry', 'LaundryController@insert')->name('newLaundry');
