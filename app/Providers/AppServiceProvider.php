74ff0576cb81b5758aa4cc8b387b75553adb0d82
tonhal <57263132+tonhal@users.noreply.github.com>
2020-02-11 23:09:36 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,10 +25,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-
-        if($this->app->environment('production'))
-        {
-            \URL::forceScheme('https');
-        }
     }
 }
