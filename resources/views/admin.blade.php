d87d97fd6c64933e806a8d2f9fa4011f6ac2af76
tonhal <57263132+tonhal@users.noreply.github.com>
2020-02-21 17:34:09 +0100

    admin view layout

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,1 +1,17 @@
-admin
+<!DOCTYPE html>
+<html lang="en" class="has-navbar-fixed-top">
+<head>
+    <title>SmartKoli - Admin</title>  
+    @include('layouts.headers')
+    <link rel="stylesheet" type="text/css" href="{{url('/css/files.css')}}">
+</head>
+<body>
+    @include('layouts.navbar')
+
+    <div id='main' class='container'>        
+       admin
+    </div>
+
+    @include('layouts.footer')
+</body>
+</html>
